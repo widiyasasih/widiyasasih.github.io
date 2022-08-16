39df32928028e3f6a1122b09f54f4df2046a4662
Widiya Sasih <widiyasasih@gmail.com>
2022-08-16 14:52:52 +0700

    Change Favicon

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -8,75 +8,39 @@ const Content = () => {
         <div className='card'>
           <h2 className='content-title'>Title</h2>
           <p className='body'>
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahs
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuhhqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
           </p>
         </div>
         <div className='card'>
           <h2 className='content-title'>Title</h2>
           <p className='body'>
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
-            Contentlhaujshuahsuaskuhskahshqashahsahsuhuahsuhasuh
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
           </p>
         </div>
       </div>
